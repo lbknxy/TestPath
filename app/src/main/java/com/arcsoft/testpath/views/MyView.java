8e9491a97f05874a23e8e118d30a09a67629f63a
lbknxy <1021138153@qq.com>
2017-05-11 15:54:26 +0800

    create a new repository

---
+++ app/src/main/java/com/arcsoft/testpath/views/MyView.java
@@ -0,0 +1,32 @@
+package com.arcsoft.testpath.views;
+
+import android.content.Context;
+import android.graphics.Paint;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lbb9756 on 2017/5/11.
+ */
+
+public class MyView extends View {
+
+
+    private Paint mPaint;
+    private Paint mPath;
+
+    private int
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
